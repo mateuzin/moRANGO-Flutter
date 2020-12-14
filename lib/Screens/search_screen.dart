88801629bb1dc5653a09b24385bea7cdb89b0d64
FelipeTorquato <felipe.camurca.torquato62@aluno.ifce.edu.br>
2020-12-14 03:23:16 -0300

    commit search_screen

--- lib/Screens/search_screen.dart
+++ lib/Screens/search_screen.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class SearchScreen extends StatefulWidget {
@@ -17,6 +18,7 @@ class _SearchScreenState extends State<SearchScreen> {
           IconButton(icon: Icon(Icons.search), onPressed: () {})
         ],
       ),
+      backgroundColor: Colors.pink[50],
       drawer: Drawer(
         child: ListView(),
       ),
@@ -24,11 +26,9 @@ class _SearchScreenState extends State<SearchScreen> {
         children: [
           Padding(
             padding: EdgeInsets.fromLTRB(10.0, 15.0, 10.0, 15.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Text("Descubra novas receitas", style: TextStyle(fontSize: 20.0)),
-              ],
+            child: Container(
+              padding: EdgeInsets.fromLTRB(10.0, 10.0, 15.0, 10.0),
+              child: Text("Descubra novas receitas!!!", style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.w500)),
             ),
           )
         ],
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:morango_app/Screens/signin_screen.dart';
+import 'package:morango_app/Screens/search_screen.dart';
 
 void main(){
   runApp(MaterialApp(
