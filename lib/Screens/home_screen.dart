758927fcc7892bfc9beff940a2b3f13c1d5c07ef
Ícaro Gabriel <icarogabriel999@gmail.com>
2020-12-21 19:07:52 -0300

    Dando um jeito no meu toc

--- lib/Screens/home_screen.dart
+++ lib/Screens/home_screen.dart
@@ -12,54 +12,9 @@ class HomeScreen extends StatelessWidget {
       drawer: CustomDrawer(),
       appBar: PreferredSize(
         child: Appbar(),
-        preferredSize: Size.fromHeight(50),
-      ),
+        preferredSize: Size.fromHeight(50),),
       backgroundColor: Color.fromRGBO(255,166,166,1),
-      body:SingleChildScrollView(
-        child: Container(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              Container(
-                padding: EdgeInsets.fromLTRB(10, 25, 0, 10),
-                child: Text("Receitas da semana",
-                  textAlign: TextAlign.start,
-
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 28,
-                    fontWeight: FontWeight.bold,
-
-                  ),),
-              ),
-              SizedBox(
-                height: 160,
-                width: 240,
-                child: Card(
-                    child: Column(children: [
-                      Image(image: AssetImage("images/Bolo-de-Chocolate-Cremoso.jpg")),
-                      RichText(
-                        text: TextSpan(text: "Peca: ", children: <TextSpan>[
-                          TextSpan(
-                            text: "5.0",
-                            style: TextStyle(color: Colors.pink[100]),
-                          )
-                        ]),
-                      ),
-                      RichText(
-                        text: TextSpan(text: "Peca: ", children: <TextSpan>[
-                          TextSpan(
-                            text: "5.0",
-                            style: TextStyle(color: Colors.pink[100]),
-                          )
-                        ]),
-                      ),
-                    ])),
-              )
-            ],
-          ),
-        ),
-      ),
+      body: Container(),
       bottomNavigationBar: BottomNavigationBar(
         backgroundColor: Color.fromRGBO(255,144,144,1),
         currentIndex: 0,
