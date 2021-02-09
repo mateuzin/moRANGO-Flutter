2a369bd0c4dee33434e6dce55cd2a0b4f26f9bc7
Luiz <senaluiz1307@gmail.com>
2021-02-09 02:00:03 -0300

    editei os forgot, coloquei o keybord type number

--- lib/screens/forgot.dart
+++ lib/screens/forgot.dart
@@ -4,172 +4,101 @@ class Forgot extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        toolbarHeight: 50,
-        backgroundColor: Color.fromRGBO(255, 144, 144, 1),
-        leading: Icon(
-          Icons.arrow_back_outlined,
-          color: Colors.white,
-          size: 35,
-        ),
-        title: Image.asset(
-          'images/logo.png',
-          width: 119,
-          height: 35,
-        ),
-        centerTitle: true,
-      ),
       backgroundColor: Color.fromRGBO(255, 166, 166, 1),
-      body: ListView(
-        children: [
-          SingleChildScrollView(
-            child: Container(
-              child: Column(
-                children: [
-                  Padding(
-                    padding: EdgeInsets.only(top: 10),
-                    child: Column(
-                      children: [
-                        Text(
-                          'ESQUEÇEU A SUA SENHA?',
-                          style: TextStyle(
-                            color: Colors.black87,
-                            fontSize: 18,
-                          ),
-                          textAlign: TextAlign.center,
-                        ),
-                        Text(
-                          'FAÇA OS PASSOS A SEGUIR:',
-                          style: TextStyle(
-                            color: Colors.black87,
-                            fontSize: 18,
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                  Padding(
-                    padding: EdgeInsets.only(top: 111, right: 81),
-                    child: Container(
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text('Insira Seu Usuário'),
-                          Container(
-                            width: 250,
-                            height: 36,
-                            child: TextField(
-                              textInputAction: TextInputAction.next,
-                              decoration: InputDecoration(
-                                enabledBorder: OutlineInputBorder(
-                                  borderRadius:
-                                  BorderRadius.all(Radius.circular(50)),
-                                  borderSide:
-                                  BorderSide(color: Colors.greenAccent),
-                                ),
-                                focusedBorder: OutlineInputBorder(
-                                  borderRadius:
-                                  BorderRadius.all(Radius.circular(50)),
-                                  borderSide:
-                                  BorderSide(color: Colors.green[900]),
-                                ),
-                                filled: true,
-                                fillColor: Colors.greenAccent,
-                                hintText: 'Usuário',
-                                hintStyle: TextStyle(
-                                    color: Color.fromRGBO(80, 73, 73, 1)),
-                              ),
-                              textAlignVertical: TextAlignVertical.bottom,
-                            ),
-                          ),
-                        ],
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            SafeArea(
+              child: Container(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.only(top: 86),
+                      child: Image.asset(
+                        'images/logo.png',
+                        width: 357,
+                        height: 105,
+                        alignment: Alignment.center,
                       ),
                     ),
-                  ),
-                  Divider(
-                    color: Color.fromRGBO(255, 166, 166, 1),
-                    height: 25,
-                  ),
-                  Padding(
-                    padding: EdgeInsets.only(right: 81),
-                    child: Container(
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text('Coloque Seu E-mail'),
-                          Container(
-                            width: 250,
-                            height: 36,
-                            child: TextField(
-                              textInputAction: TextInputAction.done,
-                              decoration: InputDecoration(
-                                enabledBorder: OutlineInputBorder(
-                                  borderRadius:
-                                  BorderRadius.all(Radius.circular(50)),
-                                  borderSide:
-                                  BorderSide(color: Colors.greenAccent),
-                                ),
-                                focusedBorder: OutlineInputBorder(
-                                  borderRadius:
-                                  BorderRadius.all(Radius.circular(50)),
-                                  borderSide:
-                                  BorderSide(color: Colors.green[900]),
-                                ),
-                                filled: true,
-                                fillColor: Colors.greenAccent,
-                                hintText: 'E-Mail',
-                                hintStyle: TextStyle(
-                                  color: Color.fromRGBO(80, 73, 73, 1),
-                                ),
-                              ),
-                              textAlignVertical: TextAlignVertical.bottom,
-                            ),
-                          ),
-                        ],
+                    Divider(
+                      height: 60,
+                      color: Color.fromRGBO(255, 166, 166, 1),
+                    ),
+                    Text(
+                      'POR FAVOR',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
                       ),
                     ),
+                    Text(
+                      'ENSIRA O SEU EMAIL NO CAMPO ABAIXO',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Divider(
+              height: 55,
+              color: Color.fromRGBO(255, 166, 166, 1),
+            ),
+            Container(
+              width: 250,
+              height: 36,
+              child: TextField(
+                decoration: InputDecoration(
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(50),
+                    ),
+                    borderSide: BorderSide(
+                      color: Color.fromRGBO(110, 213, 161, 1),
+                    ),
                   ),
-                  Divider(
-                    height: 200,
-                    color: Color.fromRGBO(255, 166, 166, 1),
-                  ),
-                  Container(
-                    width: 251,
-                    height: 51,
-                    decoration: BoxDecoration(
-                      color: Color.fromRGBO(0, 194, 122, 1),
+                  focusedBorder: OutlineInputBorder(
                       borderRadius: BorderRadius.all(
                         Radius.circular(50),
                       ),
-                    ),
-                    child: SizedBox.expand(
-                      child: FlatButton(
-                        onPressed: () {Navigator.of(context).pushNamed('/forgot2');},
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Padding(
-                              padding: EdgeInsets.only(left: 81),
-                              child: Text('SEGUINTE'),
-                            ),
-                            Icon(
-                              Icons.arrow_back,
-                              textDirection: TextDirection.rtl,
-                              color: Colors.white,
-                              size: 35,
-                            ),
-                          ],
-                        ),
-                      ),
-                    ),
-                  ),
-                ],
+                      borderSide: BorderSide(
+                        color: Color.fromRGBO(110, 213, 161, 1),
+                      )),
+                  filled: true,
+                  fillColor: Color.fromRGBO(110, 213, 161, 1),
+                ),
+                textAlign: TextAlign.center,
+                style: TextStyle(fontWeight: FontWeight.bold),
               ),
             ),
-          ),
-        ],
+            Divider(
+              height: 75,
+              color: Color.fromRGBO(255, 166, 166, 1),
+            ),
+            IconButton(
+              hoverColor: Colors.blue,
+              iconSize: 35,
+              icon: Icon(
+                Icons.arrow_back,
+                textDirection: TextDirection.rtl,
+                size: 35,
+                color: Colors.white,
+              ),
+              onPressed: () {
+                Navigator.of(context).pushNamed('/forgot2');
+              },
+            ),
+            Divider(
+              height: 45,
+              color: Color.fromRGBO(255, 166, 166, 1),
+            ),
+          ],
+        ),
       ),
     );
   }
 }
-
