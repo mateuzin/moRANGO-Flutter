1de6eff583e50aad815e9c448128fb6689181fc2
Joao Victor <joao.victor.meireles07@aluno.ifce.edu.br>
2021-02-24 00:20:07 -0300

    Ajeitada a caixinha de filtros

--- lib/widgets/filter_card.dart
+++ lib/widgets/filter_card.dart
@@ -4,13 +4,13 @@ class FilterCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      padding: EdgeInsets.fromLTRB(1, 6, 6, 0),
+      padding: EdgeInsets.fromLTRB(1, 3, 6, 0),
       margin: EdgeInsets.symmetric(vertical: 3, horizontal: 2),
       width: 100,
-      height: 34,
+      height: 30,
       decoration: BoxDecoration(
           color: Color.fromRGBO(110, 213, 161, 1),
-          borderRadius: BorderRadius.all(Radius.circular(10.0))),
+          borderRadius: BorderRadius.all(Radius.circular(50.0))),
       child: Row(
         mainAxisAlignment: MainAxisAlignment.start,
         children: [
